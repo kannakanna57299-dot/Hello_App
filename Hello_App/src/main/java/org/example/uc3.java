80017f536ee6f0f76e6aea977ac2fe16caac63ca
kannakanna57299-dot <kannakanna57299@gmail.com>
2026-04-05 09:49:39 +0530

    added uc3

---
+++ Hello_App/src/main/java/org/example/uc3.java
@@ -0,0 +1,6 @@
+public class uc3 {
+    public static void main(String[] args) {
+        String praveen = args.length > 0 ? args[0] : "";
+        System.out.println("Hello," + praveen + "!");
+    }
+}
